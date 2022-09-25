828d2b72adf88f772ba2fa6c9ddff7d7ab3197e0
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-09-25 15:01:00 -0400

    Added path to clear list

--- webserver.ts
+++ webserver.ts
@@ -4,14 +4,21 @@ let shortUrls = {
     '1111': 'https://www.google.com'
 };
 const pattern = new URLPattern({ pathname: '/:id' });
+const clear = new URLPattern({ pathname: '/clear' });
 
 serve(req => {
     try {
         if (req.method === 'POST') {
             return req.json().then(urls => {
-                shortUrls = urls;
+                shortUrls = {
+                    ...shortUrls,
+                    ...urls,
+                };
                 return Response.json(shortUrls);
             });
+        } else if (clear.test(req.url)) {
+            shortUrls = {};
+            return Response.json(shortUrls);
         } else if (pattern.test(req.url)) {
             const { id } = pattern.exec(req.url).pathname.groups;
             return shortUrls[id] ? Response.redirect(shortUrls[id], 302) : (new Response(''));
