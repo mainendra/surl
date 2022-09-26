b7dae17f4c88ffc2f6cb7086c4c29b519a4f4104
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-09-26 12:06:49 -0400

    Fixed typo

--- webserver.ts
+++ webserver.ts
@@ -4,11 +4,13 @@ import cors from "https://deno.land/x/edge_cors/src/cors.ts";
 const pattern = new URLPattern({ pathname: '/:id' });
 
 serve(async req => {
+    const request = await fetch('https://api.npoint.io/7b8cc7fda586b49d6d87');
+    const shortUrls = await request.json();
+
     if (pattern.test(req.url)) {
         const { id } = pattern.exec(req.url).pathname.groups;
         return shortUrls[id] ? Response.redirect(shortUrls[id], 302) : (new Response(''));
     }
-    const request = await fetch('https://api.npoint.io/7b8cc7fda586b49d6d87');
-    const urls = await request.json();
-    return cors(req, Response.json(urls));
+
+    return cors(req, Response.json(shortUrls));
 });
