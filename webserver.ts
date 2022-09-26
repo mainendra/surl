fdc1efb54012cfd1bdb596ef4dc8bdf4ab09fcc4
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-09-26 12:04:00 -0400

    Persistent storage

--- webserver.ts
+++ webserver.ts
@@ -1,34 +1,14 @@
 import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+import cors from "https://deno.land/x/edge_cors/src/cors.ts";
 
-let shortUrls = {
-  "1112": "https://deno.land",
-  "2222": "https://deno.com/deploy",
-  "google": "https://www.google.com",
-  "1111": "https://www.google.com"
-};
 const pattern = new URLPattern({ pathname: '/:id' });
-const clear = new URLPattern({ pathname: '/clear' });
 
-serve(req => {
-    try {
-        if (req.method === 'POST') {
-            return req.json().then(urls => {
-                shortUrls = {
-                    ...shortUrls,
-                    ...urls,
-                };
-                return Response.json(shortUrls);
-            });
-        } else if (clear.test(req.url)) {
-            shortUrls = {};
-            return Response.json(shortUrls);
-        } else if (pattern.test(req.url)) {
-            const { id } = pattern.exec(req.url).pathname.groups;
-            return shortUrls[id] ? Response.redirect(shortUrls[id], 302) : (new Response(''));
-        } else {
-            return Response.json(shortUrls);
-        }
-    } catch (err) {
-        return Response.error(err);
+serve(async req => {
+    if (pattern.test(req.url)) {
+        const { id } = pattern.exec(req.url).pathname.groups;
+        return shortUrls[id] ? Response.redirect(shortUrls[id], 302) : (new Response(''));
     }
+    const request = await fetch('https://api.npoint.io/7b8cc7fda586b49d6d87');
+    const urls = await request.json();
+    return cors(req, Response.json(urls));
 });
